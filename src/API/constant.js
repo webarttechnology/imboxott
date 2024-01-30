97e170e37d12eb5d19efaf56b922dc5c4d16ac1a
webarttechnology <jordan.digitalwebber1@gmail.com>
2024-01-30 17:11:04 +0530

    some ui

---
+++ src/API/constant.js
@@ -0,0 +1,11 @@
+//import image from "../images/noImg.png";
+
+export const CURRENCY = "$";
+export const TIMEZONE = "America/Los_Angeles";
+//export const NOIMG = image;
+export const IMG = "https://imboxo.webartechnology.com";
+// ? ====== BASE URL ======
+export const URL = "https://imboxo.webartechnology.com/api";
+
+export const SIGNUP = `${URL}/users`;
+export const LOGIN = `${URL}/login`;
